a898d40c9751bff247182f07a562896709676d37
Nakul Chandak <nakulchandak381@gmail.com>
2025-01-15 22:42:37 +0530

    Create Hello.cpp
    started C++

---
+++ Hello.cpp
@@ -0,0 +1,28 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     cout<<"hello World\n";
+// int i,j;
+
+// for(i=0;i<5;i++){
+//     for(j=0;j<5;j++){
+//         cout<<i<<j<<"\t";
+//     }
+// cout<<endl;
+// }
+//     return 0;
+// }
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string fullname;
+
+    cout<<"enter your name: ";
+    getline(cin, fullname);
+
+cout<< fullname;
+    return 0;
+}
